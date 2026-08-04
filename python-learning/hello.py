ce4203f3e74998ef50d991439a8070486517e671
gua <gua@guadeMacBook-Air.local>
2026-08-04 23:44:38 +0800

    Day 1: build AI learning evaluator

--- python-learning/hello.py
+++ python-learning/hello.py
@@ -1,9 +1,15 @@
 name=input('请输入你的名字：')
-age=int(input('请输入年龄：'))
-goal=input('请输入目标：')
+age=int(input('请输入你的年龄：'))
+hours=int(input('每天学习几小时：'))
 print(f'你好，{name}')
 print(f'你今年{age}岁')
-print(f'你的目标是成为{goal}')
-
+print(f'每天学习{hours}小时')
+if hours>=4:
+    print('优秀，坚持180天会有明显提升')
+elif hours>=2:
+    print('不错，继续保持')
+else:
+    print('建议增加学习时间')
+  
 
 
