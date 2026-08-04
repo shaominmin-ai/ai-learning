6d0b57563b03b5fbd69093970b6f5cb1e61b8018
gua <gua@guadeMacBook-Air.local>
2026-08-05 02:28:53 +0800

    Day 3 learn Python function

--- python-learning/hello.py
+++ python-learning/hello.py
@@ -1,17 +1,6 @@
-users=[
-    {
-        "name":"邵敏敏",
-        "age":31,
-        "goal":"ai应用开发"
-    },
-    {
-        "name":"小明",
-        "age":25,
-        "goal":"python开发"
-    }
-]
+def learning(skill):
+    print(f"我正在学习{skill},希望成为AI应用开发者")
 
-print(users)
-print(users[0])
-print(users[0]["name"])
-print(users[1]["goal"])
+learning("python")
+learning("RAG")
+learning("Agent")
