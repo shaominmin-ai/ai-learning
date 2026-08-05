7efc6d7934a61346b46b1318be96d4297296fcd0
gua <gua@guadeMacBook-Air.local>
2026-08-05 23:10:21 +0800

    Day 4: finish CRUD user management

--- python-learning/hello.py
+++ python-learning/hello.py
@@ -1,5 +1,23 @@
-from ai_tools import generate_answer
+import json
 
-answer=generate_answer("什么是RAG?")
+with open("users.json","r") as file:
+    users=json.load(file)
 
-print(answer)
+search_name=input("请输入要删除的用户名:")
+
+found=False
+
+
+for user in users:
+    if user["name"]==search_name:
+        users.remove(user)
+        found=True
+        break
+
+if found:
+    with open("users.json","w") as file:
+        json.dump(users,file)
+
+    print("删除成功")
+else:
+    print("没有找到用户")
